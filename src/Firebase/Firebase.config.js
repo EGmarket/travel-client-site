4b6d66a3bb501a41de607c42e0af3f2a3949a830
Asaduzzaman <ajaman375@gmail.com>
2021-10-28 12:12:35 -0700

    firebase

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDd058pCG8f0FVap-g98ymwYSnCuT-AH98",
-    authDomain: "egmarket-f2d69.firebaseapp.com",
-    projectId: "egmarket-f2d69",
-    storageBucket: "egmarket-f2d69.appspot.com",
-    messagingSenderId: "139044393161",
-    appId: "1:139044393161:web:9d9433a08625795dff3d18",
-    measurementId: "G-J5PV6970SY"
+  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 
   export default firebaseConfig;
